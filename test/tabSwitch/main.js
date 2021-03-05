9e93cf1c8d0492b8f97265d9707bc3d07c1b280c
TuringHowey <huoyihy@hotmail.com>
2021-03-05 10:42:28 -0800

    add test for tabSwitch component

---
+++ test/tabSwitch/main.js
@@ -0,0 +1,35 @@
+import {TabSwitch} from "../../component/tabSwitch/tabSwitch.js"
+import {DataStore} from "../../lib/dataStore.js"
+import {DivEle} from "../../lib/divEle.js"
+
+class TabSwitchTest extends DivEle{
+    constructor(props){
+        super(props)
+        this.store = new DataStore()
+        let tabSwitch = new TabSwitch({
+            "divId": "tabDiv",
+            "dataStore": this.store
+        })
+        this.selectionData = this.store.getData(tabSwitch.id, DataStore.subscriber(this.id, this.handleEvent))
+        tabSwitch.bindData(["optA", "optB", "optC"])
+        tabSwitch.render()
+    }
+
+    processEvent(src, event, eventObj){
+        if(eventObj.type==DataStore.dataChanged){
+            return true
+        }
+    }
+
+    outputHTML(){
+        let htmlList = []
+        htmlList.push("selected idx=[" + this.selectionData.selectedIdx + "], value=[" + this.selectionData.tabData[this.selectionData.selectedIdx] + "]")
+        this.addDivEleFrame(htmlList)
+        return htmlList
+    }
+}
+
+window.main = function main(){
+    let tabSwitch = new TabSwitchTest({"divId": "displaySelect"})
+    tabSwitch.render()
+}
