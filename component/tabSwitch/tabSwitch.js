cf1b4ed65d38ff1f3906356107058890754b6ede
Turing Compute - Huo Yi (Howey) <huoyihy@gmail.com>
2021-04-16 15:26:13 -0700

    TabSwitch remove DataStore dependency

--- component/tabSwitch/tabSwitch.js
+++ component/tabSwitch/tabSwitch.js
@@ -1,58 +1,36 @@
-import {DataStore} from "../../lib/dataStore.js"
 import {DivEle} from "../../lib/divEle.js"
 import {Format} from "../../lib/format.js"
 import {EventSrc} from "../../lib/event.js"
 
 
 class TabSwitch extends DivEle{
-    static selectionChanged = "tabSwitchSelectionChanged"
+    static Event = Object.freeze({
+        "clicked": "tabSwitchTabClicked",
+        "changed": "tabSwitchSectionChanged"
+    })
+
+    static Key = Object.freeze({
+        "tabList": "tabSwitchTabList"
+    })
 
     constructor(props){
         super(props)
-        this.dataBag = DataStore.GetStore().newData(this.id, DataStore.subscriber(this.id, this.handleEvent))
-        this.tabCache = {}
-    }
-
-    bindData(tabOpts){
-        if(!tabOpts || !Array.isArray(tabOpts) || tabOpts.length == 0){
-            throw Error("data tabOpts need to be an array, and it cannot be empty")
+        if(!props.hasOwnProperty(TabSwitch.Key.tabList) || !Array.isArray(props[TabSwitch.Key.tabList] || props[TabSwitch.Key.tabList].length==0)){
+            throw Error("property " +TabSwitch.Key.tabList + " need to be an array with data")
         }
-        this.dataBag.tabData = tabOpts
-        this.dataBag.selectedIdx = 0
-    }
-
-    selectIdx(idx){
-        if(idx != this.dataBag.selectIdx){
-            this.dataBag.selectedIdx = idx
-            DataStore.GetStore().notify(this.id)
-        }
-    }
-
-    tabDataChanged(){
-        if(!this.tabCache || !this.tabCache.hasOwnProperty("selectedIdx") || !this.tabCache.hasOwnProperty("tabData")){
-            return true
-        }
-        if(this.tabCache.selectedIdx != this.dataBag.selectedIdx){
-            return true
-        }
-        if(this.tabCache.tabData.length != this.dataBag.tabData.length){
-            return true
-        }
-        for(let idx=0; idx< this.tabCache.tabData.length; idx++){
-            if(this.tabCache.tabData[idx] != this.dataBag.tabData[idx]){
-                return true
-            }
-        }
-        return false
+        this.tabList = props[TabSwitch.Key.tabList]
+        this.selectedTab = 0
     }
 
     processEvent(eventObj){
-        if(eventObj.type == TabSwitch.selectionChanged){
-            this.selectIdx(eventObj.src)
-            return false
-        }else if(eventObj.type == DataStore.dataChanged){
-            return this.tabDataChanged()
+        if(eventObj.type == TabSwitch.Event.clicked && eventObj.src != this.selectedTab){
+            this.selectedTab = eventObj.src
+            eventObj.type = TabSwitch.Event.changed
+            eventObj.src = this.id
+            return true
         }
+        EventSrc.stop(eventObj)
+        return false
     }
 
     selectedTabValue(){
@@ -60,18 +38,18 @@ class TabSwitch extends DivEle{
     }
 
     selectedTabHtml(idx){
-        return "<td style='border-left: 1px solid rgba(204,31,48,1);border-top: 2px solid rgba(204,31,48,1);border-right: 2px solid rgba(204,31,48,1);cursor:default;'>" + this.dataBag.tabData[idx] + "</td>"
+        return "<td style='border-left: 1px solid rgba(204,31,48,1);border-top: 2px solid rgba(204,31,48,1);border-right: 2px solid rgba(204,31,48,1);cursor:default;'>" + this.tabList[idx] + "</td>"
     }
 
     optTabHtml(idx){
-        let selectEvent = EventSrc.new(TabSwitch.selectionChanged, idx, {})
-        return "<td style='border-top: 1px solid rgba(204,31,48,1);border-right: 1px solid rgba(204,31,48,1);cursor:hand;' onclick='" + this.eventTriger(selectEvent) +"'>" + this.dataBag.tabData[idx] + "</td>"
+        let selectEvent = EventSrc.new(TabSwitch.Event.clicked, idx, {})
+        return "<td style='border-top: 1px solid rgba(204,31,48,1);border-right: 1px solid rgba(204,31,48,1);cursor:hand;' onclick='" + this.eventTriger(selectEvent) +"'>" + this.tabList[idx] + "</td>"
     }
 
     outputTabHtml(){
         let tabList = []
-        for(let idx=0; idx<this.dataBag.tabData.length; idx++){
-            if(idx == this.dataBag.selectedIdx){
+        for(let idx=0; idx< this.tabList.length; idx++){
+            if(idx == this.selectedTab){
                 tabList.push(this.selectedTabHtml(idx))
             } else {
                 tabList.push(this.optTabHtml(idx))
@@ -91,8 +69,6 @@ class TabSwitch extends DivEle{
         htmlList.push(...tabList)
         htmlList.push("</table>")
         this.addDivEleFrame(htmlList)
-        this.tabCache.selectedIdx = this.dataBag.selectedIdx
-        this.tabCache.tabData = this.dataBag.tabData
         return htmlList
     }
 
--- test/tabSwitch/main.js
+++ test/tabSwitch/main.js
@@ -1,33 +1,10 @@
 import {TabSwitch} from "../../component/tabSwitch/tabSwitch.js"
-import {DataStore} from "../../lib/dataStore.js"
-import {DivEle} from "../../lib/divEle.js"
-
-class TabSwitchTest extends DivEle{
-    constructor(props){
-        super(props)
-        let tabSwitch = new TabSwitch({
-            "divId": "tabDiv"
-        })
-        this.selectionData = DataStore.GetStore().getData(tabSwitch.id, DataStore.subscriber(this.id, this.handleEvent))
-        tabSwitch.bindData(["optA", "optB", "optC"])
-        tabSwitch.render()
-    }
-
-    processEvent(src, event, eventObj){
-        if(eventObj.type==DataStore.dataChanged){
-            return true
-        }
-    }
-
-    outputHTML(){
-        let htmlList = []
-        htmlList.push("selected idx=[" + this.selectionData.selectedIdx + "], value=[" + this.selectionData.tabData[this.selectionData.selectedIdx] + "]")
-        this.addDivEleFrame(htmlList)
-        return htmlList
-    }
-}
 
 window.main = function main(){
-    let tabSwitch = new TabSwitchTest({"divId": "displaySelect"})
+    let param = {
+        "divId": "displaySelect"
+    }
+    param[TabSwitch.Key.tabList] = ["optA", "optB", "optC"]
+    let tabSwitch = new TabSwitch(param)
     tabSwitch.render()
 }
